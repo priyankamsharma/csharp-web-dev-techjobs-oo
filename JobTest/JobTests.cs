2ca0f5baf2391315d8d78543341d9de28aa0bdf0
Priyanka <priyanka.msharma@gmail.com>
2022-05-02 17:54:23 -0400

    Completed UT until TDD

--- JobTest/JobTests.cs
+++ JobTest/JobTests.cs
@@ -1,13 +1,55 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
 
 namespace JobTest
 {
     [TestClass]
-    public class UnitTest1
+    public class JobTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestSettingJobId()
         {
+            Job Job1 = new Job();
+            Job Job2 = new Job();
+
+            //tests ID values are not the same
+            Assert.IsFalse(Job1.Equals(Job2));
+
+            //tests ID values differ by 1
+            Assert.AreEqual((Job2.Id - 1), (Job1.Id));
         }
-    }
+
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Job Job3 = new Job(1, "Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+            //tests that the constructor correctly assigns Name to "Product tester"
+            Assert.IsTrue(Job3.Name == "Product tester");
+
+            //tests that the constructor correctly assigns EmployerName to "ACME"
+            Assert.IsTrue(Job3.EmployerName.Value == "ACME");
+
+            //tests that the constructor correctly assigns EmployerLocation to "Desert"
+            Assert.IsTrue(Job3.EmployerLocation.Value == "Desert");
+
+            //tests that the constructor correctly assigns JobType to "Quality control"
+            Assert.IsTrue(Job3.JobType.Value == "Quality control");
+
+            //tests that the constructor correctly assigns JobCoreCompetency to "Persistence"
+            Assert.IsTrue(Job3.JobCoreCompetency.Equals("Persistence"));
+        }
+
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Job Job4 = new Job(1, "Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Job Job5 = new Job(2, "Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+            //tests id values are not the same
+            Assert.IsFalse(Job4.Equals(Job5));
+        }
+
+    } 
 }
--- TechJobsOO/CoreCompetency.cs
+++ TechJobsOO/CoreCompetency.cs
@@ -1,40 +1,40 @@
 ﻿using System;
+
 namespace TechJobsOO
 {
-    public class CoreCompetency
+public class CoreCompetency
+{
+    private int Id;
+    private static int nextId = 1;
+    private string Value;
+
+    // TODO: Change the fields to auto-implemented properties.
+
+    public CoreCompetency()
     {
-        private int id;
-        private static int nextId = 1;
-        private string value;
-
-        // TODO: Change the fields to auto-implemented properties.
-
-        public CoreCompetency()
-        {
-            id = nextId;
-            nextId++;
-        }
-
-        public CoreCompetency(string v) : this()
-        {
-            value = v;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is CoreCompetency competency &&
-                   id == competency.id;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(id);
-        }
-
-        public override string ToString()
-        {
-            return value;
-        }
+        Id = nextId;
+        nextId++;
     }
+
+    public CoreCompetency(string value) : this()
+    {
+        Value = value;
     }
+
+    public override bool Equals(object obj)
+    {
+        return obj is CoreCompetency competency &&
+               Id == competency.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id);
+    }
+
+    public override string ToString()
+    {
+        return Value;
+    }
+}
 }
